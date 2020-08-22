24692641b76e7342bc749286ef8dc6d02e082a36
Aishah Bushra <aishahbushra@outlook.com>
2020-08-22 11:36:25 +0100

    pet gets older and keeps fit

--- src/pet.js
+++ src/pet.js
@@ -1,8 +1,28 @@
-function Pet(name) {
-  this.name = name
-}
+const MAXIMUM_FITNESS = 10;
 
-const pet = new Pet('Fido')
-    this.name = 'Fido';
+function Pet(name) {
+  this.name = name,
+  this.age = 0
+  this.hunger = 0
+  this.fitness = MAXIMUM_FITNESS
+}
+Pet.prototype.growUp = function() {
+    this.age += 1;
+    this.hunger = this.age * 5;
+    this.fitness = MAXIMUM_FITNESS - (3 * this.age);
+}
+Pet.prototype.walk = function() {
+  if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
+    this.fitness += 4;
+  } else {
+    this.fitness = MAXIMUM_FITNESS;
+  }
+}
+//
+
+{ const myPet = new Pet('Gavin')
+    this.name = 'Gavin'
+}
+    
 
 module.exports = Pet;
---
+++ test/pet.test.js
@@ -0,0 +1,103 @@
+const Pet = require('../src/pet');
+
+describe('constructor', () => {
+    it('returns an object', () => {
+      expect(new Pet('Gavin')).toBeInstanceOf(Object);
+    });
+  });
+
+  describe('constructor', () => {
+  
+    it('sets the name property', () => {
+      const myPet = new Pet('Gavin');
+  
+      expect(myPet.name).toEqual('Gavin');
+    });
+  });
+
+  describe('constructor', () => {
+  
+    it('has a initial age of 0', () => {
+      const myPet = new Pet('Gavin');
+  
+      expect(myPet.age).toEqual(0);
+    });
+  });
+
+  describe('growUp', () => {
+
+    it('increases age by 1 year at a time', () => {
+      const myPet = new Pet('Gavin');
+
+      myPet.growUp();
+
+      expect(myPet.age).toBe(1);
+
+    });
+  });
+
+  describe('constructor', () => {
+
+    it('has initial hunger of 0', () => {
+      const myPet = new Pet('Gavin');
+
+      expect(myPet.hunger).toBe(0);
+    });
+  });
+
+  describe('growUp', () => {
+
+    it('increases hunger by 5 each time', () => {
+      const myPet = new Pet('Gavin');
+
+      myPet.growUp();
+
+      expect(myPet.hunger).toBe(5);
+
+    });
+  });
+
+  describe('constructor', () => {
+
+    it('has initial fitness of 0', () => {
+      const myPet = new Pet('Gavin');
+
+      expect(myPet.fitness).toBe(10);
+    });
+  });
+
+  describe('growUp', () => {
+
+    it('reduces fitness by 3 each time', () => {
+      const myPet = new Pet('Gavin')
+
+      myPet.growUp();
+
+      expect(myPet.fitness).toBe(7);
+
+    });
+  });
+
+  describe('walk', () => {
+
+    it('increases fitness level by 4 each time', () => {
+      const myPet = new Pet ('Gavin')
+
+      myPet.fitness = 4;
+      myPet.walk();
+
+      expect(myPet.fitness).toBe(8);
+    });
+  });
+
+  describe('walk', () => {
+  
+    it('increases fitness by to a maximum of 10', () => {
+      const myPet = new Pet('fido');
+  
+      myPet.fitness = 8;
+      myPet.walk();
+  
+      expect(myPet.fitness).toEqual(10);
+    });
+  });
